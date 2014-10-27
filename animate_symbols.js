d5f03398fe166a43e4fb00ccbe34771e9b19ac67
Sagar Junnarkar <sagarjunnu@gmail.com>
2014-10-27 20:52:58 +0530

    rotate zoom

--- animate_symbols.js
+++ animate_symbols.js
@@ -1,6 +1,29 @@
 $(window).load(function() {
+// var angle = 0;     // starting position (degrees)
+// var distance = 250; // distance of b from a
+// var speed = 10;    // revolution speed in degrees per second
+// var rate  = 10;    // refresh rate in ms
+
+// function f() {
+    
+//     var o = $('.centered').offset();
+    
+//     var t = o.top + (distance * Math.sin(angle * Math.PI/180.0));
+//     var l = o.left+ (distance * Math.cos(angle * Math.PI/180.0));
+    
+//     $('.mother_symbol').css({
+//         top: t,
+//         left: l
+//     });
+    
+//     angle += (speed * (rate/1000)) % 360;
+// }
+
+// setInterval(f, rate);
+
   screen_width = $( window ).width()
   screen_height = $( window ).height()
+  $('div').css({'min-height': screen_height})
   mother_symbol_images = []
   aurobindo_symbol = $('img.centered')[0]
   centeredImagePostion = {
@@ -9,9 +32,15 @@ $(window).load(function() {
                            right:(screen_width/2) + (aurobindo_symbol.width/2),//aurobindo_symbol.offsetLeft + 358,
                            bottom:(screen_height/2) + (aurobindo_symbol.height/2) //aurobindo_symbol.offsetTop + 333,
                          }
+  h = -2                       
   function getNewPosition(){
-    x = Math.floor(Math.random () * (screen_width - 115))
-    y = Math.floor(Math.random () * (screen_height - 115))
+    x = Math.floor(Math.random () * (screen_width - (aurobindo_symbol.width/2)))
+    if (h==-2) {
+      h = 2
+    } else{
+      h = -2
+    }; 
+    y = Math.floor(Math.random () * (screen_height*h - (aurobindo_symbol.height/2)))
     return [x,y]
   }
   newPosition = [0,0]
@@ -30,16 +59,17 @@ $(window).load(function() {
                             })
   function drawImage(){
     var img = $('<img class="mother_symbol">');
-    img.attr('src', 'mother_symbol.png');
+    img.attr('src', 'Mother\'s Symbol Large.png');
     img.css({
       "left": (newPosition[0]) + "px",
       "top": (newPosition[1]) + "px",
       "width": (dimensions[0]) + "px",
       "height": (dimensions[1]) + "px",
-      "display": "none"
+      "display": "none",
+      "position": "absolute"
     });
     img.css('opacity', 1)
-    img.appendTo('body')
+    img.appendTo('div')
   }
     for (var i = 0; i < 1000; i++) {
       newPosition = getNewPosition()
@@ -69,20 +99,21 @@ $(window).load(function() {
           return false
           }
       });
-      if($('img').length == 50 || i == 999){
-        mother_symbols = $('img.mother_symbol')
-        one_forth = Math.floor(mother_symbols.length/4)
-        mother_symbols.slice(0,one_forth).fadeIn(1000)
-        mother_symbols.slice(one_forth+1,one_forth*2).delay(1000).fadeIn(5000)
-        mother_symbols.slice((one_forth*2) +1,(one_forth*3)).delay(2000).fadeIn(5000)
-        mother_symbols.slice((one_forth*3) +1,(one_forth*4)).delay(3000).fadeIn(5000)
+      if($('img').length == 500 || i == 999){
+        mother_symbols = $('img.mother_symbol').fadeIn(5000)
+        // one_forth = Math.floor(mother_symbols.length/4)
+        // mother_symbols.slice(0,one_forth).fadeIn(2000)
+        // mother_symbols.slice(one_forth+1,one_forth*2).delay(1000).fadeIn(5000)
+        // mother_symbols.slice((one_forth*2) +1,(one_forth*3)).delay(2000).fadeIn(5000)
+        // mother_symbols.slice((one_forth*3) +1,(one_forth*4)).delay(3000).fadeIn(5000)
+//        $('.mother_symbol').animate({left: 100}, 2000).animate({top:  100}, 2000).animate({left:   0}, 2000).animate({top:    0}, 2000);
         return false;
       }
     };
     function intersectRect(r1, r2) {
-      return !(r2.left > r1.right || 
-      r2.right < r1.left || 
-      r2.top > r1.bottom ||
-      r2.bottom < r1.top);
+      return !((r2.left - 20) > r1.right || 
+      (r2.right + 20) < r1.left || 
+      (r2.top - 20) > r1.bottom ||
+      (r2.bottom + 20) < r1.top);
     }
 });
